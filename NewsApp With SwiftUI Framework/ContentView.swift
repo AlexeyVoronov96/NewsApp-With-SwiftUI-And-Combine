1115fb3ed02d0fe9d3177e54e49459c83dcadf48
Alexey <alexey.voronov96@gmail.com>
2019-06-15 13:15:09 +0500

    Initial Commit

---
+++ NewsApp With SwiftUI Framework/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NewsApp With SwiftUI Framework
+//
+//  Created by Алексей Воронов on 15.06.2019.
+//  Copyright © 2019 Алексей Воронов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
