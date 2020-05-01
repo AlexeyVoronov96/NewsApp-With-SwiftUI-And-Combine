11a4ad0320531500d0cf22a7377200328c696946
Alexey <alexey.voronov96@gmail.com>
2020-05-01 23:43:30 +0500

    refactoring

--- NewsApp With SwiftUI Framework/UI/Weather/WeatherViewModel.swift
+++ NewsApp With SwiftUI Framework/UI/Weather/WeatherViewModel.swift
@@ -17,15 +17,6 @@ final class WeatherViewModel: ObservableObject {
     @Published private (set) var weather: WeatherResponse?
     @Published private (set) var locationName: String = Constants.gettingYourLocationText
     
-    var emptyWeather: WeatherResponse {
-        return WeatherResponse(currently: Weather(time: Date(),
-                                                  icon: .clearDay,
-                                                  temperature: 0.0),
-                               hourly: HourlyWeatherData(summary: "",
-                                                         data: []),
-                               daily: DailyWeatherData(data: []))
-    }
-    
     func getCityName() {
         weatherService.getCityName { [weak self] result in
             guard let self = self else { return }
@@ -42,11 +33,15 @@ final class WeatherViewModel: ObservableObject {
     func getCurrentWeather() {
         weatherService.requestCurrentWeather()
             .decode(type: WeatherResponse.self, decoder: Container.weatherJSONDecoder)
-            .replaceError(with: emptyWeather)
             .receive(on: RunLoop.main)
-            .sink(receiveValue: { [weak self] weather in
-                self?.weather = weather
-            })
+            .sink(
+                receiveCompletion: { [weak self] _ in
+                    self?.weather = nil
+                },
+                receiveValue: { [weak self] weather in
+                    self?.weather = weather
+                }
+            )
             .store(in: &bag)
     }
 }
